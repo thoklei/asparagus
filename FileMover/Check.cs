074481a49486a5d498f397a5bb22ae80d1b5dff9
Richard <rruppel@uni-osnabrueck.de>
2019-05-06 23:33:14 +0200

    git

--- FileMover/Check.cs
+++ FileMover/Check.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Topshelf;
 
@@ -12,22 +13,46 @@ namespace DirCheck
     class Check
     {
         private FileSystemWatcher watcher;
-        private string destinationFolder = @"C:\Users\Richard\Desktop\Folder1";
-        private string sourceFolder = @"C:\Users\Richard\Desktop\Folder2";
-
-        public Check()
+        private string sourceFolder;
+        private string destinationFolder;
+        
+        // custom constructor
+        public Check(string sourceFolder, string destinationFolder)
         {
+            this.sourceFolder = sourceFolder;
+            this.destinationFolder = destinationFolder;
+            
             watcher = new FileSystemWatcher(sourceFolder);
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
-            //watcher.NotifyFilter = NotifyFilters.LastWrite;;
-
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastAccess | NotifyFilters.LastWrite;
+            
         }
+
+        private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("At {0} file created: {1}", e.FullPath, e.Name, e.ChangeType.ToString());
+        }
+
+        private static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("At {0} file deleted: {1}", e.FullPath, e.Name);
+        }
+
+        private static void FileSystemWatcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("At {0} file renamed {1}", e.FullPath, e.Name);
+        }
+        
 
         public void Start()
         {
             watcher.Created += OnChanged;
             watcher.Changed += OnChanged;
+            watcher.Renamed += OnChanged;
 
+            watcher.Created += FileSystemWatcher_Created;
+            watcher.Deleted += FileSystemWatcher_Deleted;
+            watcher.Renamed += FileSystemWatcher_Renamed;
+            
             watcher.EnableRaisingEvents = true;
             watcher.IncludeSubdirectories = true;
         }
@@ -38,10 +63,14 @@ namespace DirCheck
         {
             foreach (string file in Directory.GetFiles(sourceFolder, "*.*", SearchOption.AllDirectories))
             {
-                Console.WriteLine(file +"  >  " + Path.Combine(destinationFolder , Path.GetFileName(file)) );
-                File.Move(file, Path.Combine(destinationFolder, Path.GetFileName(file)));
+                FileInfo fi = new FileInfo(file);
+
+                if (fi.LastAccessTime < DateTime.Now.AddMinutes(-2))
+                {
+                    Console.WriteLine(file + "  >  " + Path.Combine(destinationFolder, Path.GetFileName(file)));
+                    File.Move(file, Path.Combine(destinationFolder, Path.GetFileName(file)));
+                }
             }
-        
         }
 
         public void Stop()
--- FileMover/Program.cs
+++ FileMover/Program.cs
@@ -87,19 +87,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileMover;
 using Topshelf;
 
 namespace DirCheck
 {
     class Program
     {
+        //public static string sourceFolder = @"C:\Users\Richard\Desktop\source";
+        public static string sourceFolder = @"C:\Users\Hermeler\Desktop\source";
+        // public static string destinationFolder = @"C:\Users\Richard\Desktop\destination";
+        public static string destinationFolder = @"C:\Users\Hermeler\Desktop\destination";
+
+
         static void Main(string[] args)
         {
+            FileGenerator fileGenerator = new FileGenerator();
+            fileGenerator.Run();
+
             var exitCode = HostFactory.Run(x =>
             {
                 x.Service<Check>(s =>
                 {
-                    s.ConstructUsing(check => new Check());
+                    s.ConstructUsing(check => new Check(sourceFolder, destinationFolder));
                     s.WhenStarted(check => check.Start());
                     s.WhenStopped(check => check.Stop());
                 });
